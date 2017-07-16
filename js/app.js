bc6e287afe518f696df9096eaa5e1053fee2e748
aplusadrama <aplusadrama@naver.com>
2017-07-16 17:50:51 +0900

    to be continued..

--- js/app.js
+++ js/app.js
@@ -36,17 +36,73 @@
 //   console.log("You are getting old");
 // }
 
+// var weaponsList = ["AK-47", "Gun", "Machete", "Knife"];
+// var countries = ["Korea", "Japan", "Greece", "Colombia"];
+//
+// var soldier = {
+//   name: "Nicolas",
+//   weapon: weaponsList[2],
+//   country: countries[3],
+//   isDead: false,
+//   shoot: function() {
+//     console.log("Pew pew pew");
+//   },
+//   sayHi: function(sargent) {
+//     console.log("Hello " + sargent + " my name is " + this.name);
+//   }
+// };
+// function nameOfTheFunction(arguments){
+//   instructions
+// }
+//
+// function multiply(chicken, potato) {
+//   return chicken * potato;
+// }
+//
+// function division(lala, lolo) {
+//   return lala / lolo;
+// }
+// var age = prompt("How old are you?");
+//
+// if (age < 18) {
+//   console.log("You can't drink");
+// } else if (age > 18 && age < 45) {
+//   console.log("You're an adult");
+// } else {
+//   console.log("You are getting old");
+// }
+//
+// true && true = true
+// false && true = false
+// true && false = false
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
 
 $(document).ready(function() {
-  var $heart = $(".heart");
+  var $heart = $(".heart"),
+    $comment = $(".comment"),
+    $overlay = $(".profileOverlay"),
+    $cog = $(".fa-cog"),
+    $closeIcon = $(".profileOverlay i"),
+    $cancelBtn = $("#cancel");
   $heart.click(function() {
-    $(this).toggleClass("fa-heart-o")
+    $(this).toggleClass("fa-heart-o");
     $(this).toggleClass("fa-heart heart-click");
   });
-
-  var $comment = $(".comment");
   $comment.click(function() {
-    $(this).toggleClass("fa-comment-o")
-    $(this).toggleClass("fa-comment comment-click")
+    $(this).toggleClass("fa-comment-o");
+    $(this).toggleClass("fa-comment comment-click");
   });
+  $cog.click(function() {
+    $overlay.fadeIn(500);
+  });
+
+  $closeIcon.click(closeOverlay);
+  $cancelBtn.click(closeOverlay);
+
+  function closeOverlay() {
+    $overlay.fadeOut(500);
+  }
 });
